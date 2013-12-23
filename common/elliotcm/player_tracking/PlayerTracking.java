a1994f5b6d4409978381e0b89aa23a2d108f522e
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2013-12-23 04:11:39 +0000

    Add skeleton code for tracking mod.

---
+++ common/elliotcm/player_tracking/PlayerTracking.java
@@ -0,0 +1,32 @@
+package elliotcm.player_tracking;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid = "ecmPlayerTracking", name = "elliotcm Player Tracking", version = "0.0.1")
+@NetworkMod(channels = {"ecmPlayerTracking"}, clientSideRequired = false, serverSideRequired = false)
+public class PlayerTracking {
+
+	@Instance("ecmPlayerTracking")
+	public static PlayerTracking mod;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		
+	}
+}
